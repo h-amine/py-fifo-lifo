d278329eb0c443e69a0da75323c6553d8d31dbfe
h-amine <amine.hmidi0.1@gmail.com>
2021-12-27 01:22:45 +0100

    Added python's primitive data structures to the TEST_LIST. Added requirements.txt file.

--- tests/data_test.py
+++ tests/data_test.py
@@ -1,11 +1,26 @@
-import string
+import random
+import array
 
 
-DEFAULT_LIST_LEN = 10_000
+DEFAULT_LIST_LEN = 1_000_000
 
 TEST_LISTS = [
     # integers
     [x for x in range(0, DEFAULT_LIST_LEN)],
-    # alphanumeric strings
-    [x for x in list(string.ascii_lowercase + string.ascii_uppercase + string.digits)]
+    # floats
+    [float(x) for x in range(0, DEFAULT_LIST_LEN)],
+    # strings
+    [str(x) for x in range(0, DEFAULT_LIST_LEN)],
+    # booleans
+    [bool(random.getrandbits(1)) for x in range(0, DEFAULT_LIST_LEN)],
+    # arrays
+    [array.array("I", [x]) for x in range(0, DEFAULT_LIST_LEN)],
+    # lists
+    [[x] for x in range(0, DEFAULT_LIST_LEN)],
+    # tuples
+    [(x, x-1) for x in range(0, DEFAULT_LIST_LEN)],
+    # dictionaries
+    [{str(x): x} for x in range(0, DEFAULT_LIST_LEN)],
+    # sets
+    [set(str(x)) for x in range(0, DEFAULT_LIST_LEN)]
 ]
